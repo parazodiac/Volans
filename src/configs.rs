5390b6b343dfff7b181de75fbc57f3f6b840ebf6
Avi Srivastava <asrivastava@cs.stonybrook.edu>
2020-11-05 14:59:12 -0500

    interim changes

---
+++ src/configs.rs
@@ -0,0 +1,16 @@
+pub const MIL: usize = 1_000_000;
+pub const TMIL: usize = 10_000_000;
+pub const HMIL: usize = 100_000_000;
+
+pub const FRAG_DIST: i64 = 10;
+pub const MATE_MIN_DISTANCE: i64 = 20;
+pub const MATE_MAX_DISTANCE: i64 = 662;
+pub const MIN_MAPQ: u8 = 30;
+pub const CB_LENGTH: usize = 16;
+pub const TN5_LEFT_OFFSET: i64 = 4;
+pub const TN5_RIGHT_OFFSET: i64 = 5;
+pub const IS_WTL_FWD: bool = true;
+pub const NUM_SUPPORT_CB: u32 = 5;
+pub const MIN_FEAT_COUNT: u64 = 5;
+pub const WINDOW_SIZE: i64 = 500;
+pub const PILEUP_THRESHOLD: u16 = 15;
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ extern crate bitvector;
 extern crate clap;
 extern crate indicatif;
 extern crate itertools;
-extern crate libradicl;
 extern crate num_format;
 extern crate pretty_env_logger;
 extern crate quickersort;
@@ -21,25 +20,8 @@ use std::error::Error;
 pub mod mapping;
 pub mod fragmentation;
 
-pub const MIL: usize = 1_000_000;
-pub const TMIL: usize = 10_000_000;
-pub const HMIL: usize = 100_000_000;
-
-pub const FRAG_DIST: i64 = 10;
-pub const MATE_MIN_DISTANCE: i64 = 20;
-pub const MATE_MAX_DISTANCE: i64 = 662;
-pub const MIN_MAPQ: u8 = 30;
-pub const CB_LENGTH: usize = 16;
-pub const TN5_LEFT_OFFSET: i64 = 4;
-pub const TN5_RIGHT_OFFSET: i64 = 5;
-pub const IS_WTL_FWD: bool = true;
-pub const NUM_SUPPORT_CB: u32 = 5;
-pub const MIN_FEAT_COUNT: u64 = 5;
-pub const WINDOW_SIZE: i64 = 500;
-pub const PILEUP_THRESHOLD: u16 = 15;
-
 fn main() -> Result<(), Box<dyn Error>> {
-    let matches = App::new("flash")
+    let matches = App::new("volans")
         .version("0.1.0")
         .author("Avi Srivastava, Tim Stuart, Bingjie Zhang, Rahul Satija")
         .about("A set of fast helper functions for Cut&Tag/ATAC data analysis.")
